2cd9a45089a8e3433515263df9e974cf1e4c002d
JYLeem <102199863+JYLeem@users.noreply.github.com>
2023-01-11 01:45:30 +0900

    [Bronze II] Title: 상수, Time: 120 ms, Memory: 14108 KB -BaekjoonHub

---
+++ 백준/Bronze/2908. 상수/상수.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int first = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int second = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.println(Math.max(first, second));
+    }
+}
