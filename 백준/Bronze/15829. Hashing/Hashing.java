f0521288404d20968cc61f734bcc5371cfd64a51
JYLeem <102199863+JYLeem@users.noreply.github.com>
2023-01-14 22:31:13 +0900

    [Bronze II] Title: Hashing, Time: 120 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -9,8 +9,17 @@ public class Main {
         String s = br.readLine();
         long result = 0;
 
-        for (int i = 0; i < L; ++i){
-            result += (long)(s.charAt(i) - 'a' + 1) * (Math.pow(31, i));
+
+        if (L == 1){
+            result = (s.charAt(0) - 'a' + 1);
+
+        } else {
+            for (int i = 0; i < L/2; ++i){
+                result += (s.charAt(i) - 'a' + 1) * (Math.pow(31, i));
+            }
+            for (int j = L/2+1; j <= L; ++j) {
+                result += (s.charAt(j - 1) - 'a' + 1) * (Math.pow(31, j - 1));
+            }
         }
         System.out.println(result);
     }
