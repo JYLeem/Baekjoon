59d0a946981f82f2b4ab83720aa1afe425a5d838
JYLeem <102199863+JYLeem@users.noreply.github.com>
2023-01-14 19:28:57 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        String s = br.readLine();
+        int result = 0;
+
+        for (int i = 0; i < L; ++i){
+            result += (int)(s.charAt(i) - 'a' + 1) * (Math.pow(31, i));
+        }
+        System.out.println(result);
+    }
+}
