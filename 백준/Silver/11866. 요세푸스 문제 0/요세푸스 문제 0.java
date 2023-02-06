9f57724cd12e3753a4295cfd17a38f7908cc0496
JYLeem <102199863+JYLeem@users.noreply.github.com>
2023-02-06 11:58:29 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,42 +1,34 @@
 import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
- 
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		LinkedList<Integer> list = new LinkedList<Integer>();
-		
-		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		for(int i = 1; i <= N; i++) {
-			list.add(i);
-		}
-		
-		
-		StringBuilder sb = new StringBuilder();
-		sb.append('<');
-		
-		int index = 0;	// 리스트에서 삭제할 요소를 참조할 인덱스 변수
-		
-		while(N > 1) {
-			index = (index + (K - 1)) % N;
-			
-			// index위치에 있는 요소를 삭제함과 동시에 출력 
-			sb.append(list.remove(index)).append(", "); 
-			N--;
-		}
-		
-		// 마지막으로 남은 요소 삭제함과 동시에 출력
-		sb.append(list.remove()).append('>');
-		System.out.println(sb);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int N = Integer.parseInt(st.nextToken());
+        int T = Integer.parseInt(st.nextToken());
+        LinkedList<Integer> arr = new LinkedList<Integer>();
+
+
+        for (int i = 1; i <= N; ++i){
+            arr.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("<");
+        int k = 0;
+
+        while (N > 1){
+            k = (k + (T-1)) % N;
+            sb.append(arr.remove(k)).append(", ");
+            N--;
+
+        }
+
+        sb.append(arr.remove()).append('>');
+        System.out.println(sb);
+    }
 }
