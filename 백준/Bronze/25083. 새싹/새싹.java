24cc3d6c5ca3209037876108883b07739b3ada82
JYLeem <102199863+JYLeem@users.noreply.github.com>
2024-05-21 10:08:00 +0900

    [Bronze V] Title: 새싹, Time: 116 ms, Memory: 13892 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
