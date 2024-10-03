7898eefbbbb01b7bd64524443bd86f160f8cb0da
JYLeem <102199863+JYLeem@users.noreply.github.com>
2024-10-03 20:32:54 +0900

    [Gold V] Title: LCS, Time: 136 ms, Memory: 18568 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        String word1 = br.readLine();
+        String word2 = br.readLine();
+        
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        
+        for (int i = 1; i <= word1.length(); i++) {
+            for (int j = 1; j <= word2.length(); j++) {
+                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        
+        System.out.println(dp[word1.length()][word2.length()]);
+    }
+}
